1e81344824bf50a1abe2bc35a94fe852a36ce276
Izsaah <longxautrai12@gmail.com>
2025-10-07 23:35:39 +0700

    Fix comparevehicle

--- BE/src/main/java/model/controller/CompareModelFeaturesController.java
+++ BE/src/main/java/model/controller/CompareModelFeaturesController.java
@@ -10,6 +10,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 import model.service.CompareModelFeaturesService;
 import model.service.ViewVehicleService;
 import utils.ResponseUtils;
@@ -19,15 +20,26 @@ import utils.ResponseUtils;
  * @author ACER
  */
 @WebServlet("/api/public/compareVehicle")
-public class CompareModelFeaturesController extends HttpServlet{
-      private final CompareModelFeaturesService service = new CompareModelFeaturesService();
-   
+public class CompareModelFeaturesController extends HttpServlet {
+
+    private final CompareModelFeaturesService service = new CompareModelFeaturesService();
+
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         try {
-            String VehicleName=req.getParameter("vehicleName");
-            ResponseUtils.success(resp, "success", service.HandlingSearchVehicleByVehicleName(VehicleName));
+            String VehicleName = req.getParameter("vehicleName");
+            
+            Object searchResult = service.HandlingSearchVehicleByVehicleName(VehicleName);
+
+            if (searchResult != null && !((List<?>) searchResult).isEmpty()) {
+              
+                ResponseUtils.success(resp, "success", searchResult);
+            } else {
+
+                ResponseUtils.error(resp, "No vehicles found with that name.");
+
+            }
         } catch (Exception e) {
             e.printStackTrace();
             ResponseUtils.error(resp, "An error occurred while retrieving vehicles: " + e.getMessage());
